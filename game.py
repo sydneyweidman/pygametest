e600f1d21edcd14a0968883b5a4fc64bba416020
Sydney Weidman <sydney.weidman@gmail.com>
2015-03-12 05:40:23 -0500

    Added rudimentary message display mechanism

--- game.py
+++ game.py
@@ -2,140 +2,75 @@ import optparse
 import sys
 import pygame
 from pygame.locals import QUIT, KEYDOWN, MOUSEBUTTONDOWN
-from pygame.locals import K_n, K_q, K_s, K_p
-from mapdata import HOME, stars, BLUE, GREEN
+from pygame.locals import K_n, K_q, K_r, K_s, K_p, K_ESCAPE
+from pygame.locals import K_0, K_1, K_2, K_3, K_4, K_5, K_6, K_7, K_8
+from mapdata import stars, ring, tokens, BLUE, GREEN, BLACK, WHITE, COLOR_VALUE, STATE
 
 parser = optparse.OptionParser()
 
-WINDOW = pygame.Rect(0, 0, 690, 710)
+H = 800
+W = 690
+H_BGND = 710
+H_MSG = 90
+
+TOKEN_SIZE = 40
+SLOT_SIZE = 20
+FONT_SIZE = 32
+
+NUMKEYS = [K_0, K_1, K_2, K_3, K_4, K_5, K_6, K_7, K_8]
+
 
 class Slot(pygame.Rect):
-
-    def __init__(self, stype, top, left, width=40, height=40, nextslot=None, prevslot=None, ctrslot=None, token=None):
+    def __init__(self, top, left, width=SLOT_SIZE, height=SLOT_SIZE,
+                 nextslot=None, prevslot=None, ctrslot=None, token=None):
         super(Slot, self).__init__((top, left, width, height))
-        self.stype = stype
         self.nextslot = nextslot
         self.prevslot = prevslot
         self.ctrslot = ctrslot
         self.token = token
 
-class GameBoard(object):
-    """A game board
-    """
 
-    def __init__(self, mode=(690,710)):
-        """Create and initialize the game board
-        """
-        self.screen = pygame.display.set_mode(mode)
-        self.background = pygame.image.load('images/board.png').convert()
+class Board(object):
+    """a collection of spaces through which a token can move"""
+
+    def __init__(self):
         self.slots = []
-        self.tokens = {}
         self._setup_slots()
-        self._setup_tokens()
-        self.current_color = BLUE
-        self.active_token = self.tokens[self.current_color].sprites()[0]
-        self.active_token.selected = True
-        self.current_slot = self.slots[0]
-        
+
     def _setup_slots(self):
-        for s in stars:
-            if s != 'center':
-                self.slots.append(Slot('ring', *stars[s][0]))
-            else:
-                self.slots.append(Slot('center', *stars['center'][0]))
+        center = Slot(*stars['center'])
+        self.slots.append(center)
+        for s in ring:
+            self.slots.append(Slot(*stars[s]))
         for idx, slot in enumerate(self.slots):
-            slot.nextslot = self.slots[(idx + 1) % 8]
-            slot.prevslot = self.slots[(idx - 1) % 8]
+            slot.nextslot = self.slots[(idx + 1) % len(self.slots)]
+            slot.prevslot = self.slots[(idx - 1) % len(self.slots)]
+            slot.ctrslot = center
 
-    def _setup_tokens(self):
-        # set up tokens
-        for color in HOME:
-            self.tokens[color] = pygame.sprite.Group()
-            for tokenpos in HOME[color]:
-                token = Token(self.screen, color, tokenpos)
-                self.tokens[color].add(token)
 
-    def onKeyDown(self, event):
-        """Handle keyboard events"""
-        if event.key == K_s:
-            print "Starting game"
-            return
-        if event.key == K_n:
-            if self.current_slot.nextslot.token:
-                print "That space is occupied"
-                return
-            self.current_slot = self.current_slot.nextslot
-            self.screen.blit(self.background, (0,0))
-            self.active_token.move((self.current_slot.x, self.current_slot.y))
-            self.active_token.draw()
-            return
-        if event.key == K_p:
-            if self.current_slot.prevslot.token:
-                print "That space is occupied"
-                return
-            self.current_slot = self.current_slot.prevslot
-            self.screen.blit(self.background, (0,0))
-            self.active_token.move((self.current_slot.x, self.current_slot.y))
-            self.active_token.draw()
-            return
-        if event.key == K_q:
-            self.onQuit(event)
-            
-    def onMouseButtonDown(self, event):
-        """Handle mouse events"""
-        print "Click: %s" % (event.pos,)
-        # if a token has been selected previously, move it to the destination of the click
-        # as long as the spot is not occupied
-        if self.active_token:
-            if self.active_token.selected:
-                self.active_token.selected = False
-            for slot in self.slots:
-                print "Slot %s" % slot
-                if slot.collidepoint(event.pos):
-                    print "Hit slot"
-                    if slot.token:
-                        print "That space is occupied"
-                        return
-                    self.active_token.move((slot.x, slot.y))
-                    slot.token = self.active_token
-                    self.active_token.selected = False
-                    self.active_token = None
-                    break
-        else:
-            for color in HOME:
-                for token in self.tokens[color]:
-                    if token.rect.collidepoint(event.pos):
-                        print "hit"
-                        self.active_token = token
-                        token.selected = True
-        return
-
-    def onQuit(self, event):
-        sys.exit()
-        
 class Token(pygame.sprite.Sprite):
     """A peg in the board. 3 Tokens are placed on the board at the beginning of the game.
     """
-    radius = 20
+    radius = int(TOKEN_SIZE / 2.0)
 
     def __init__(self, screen, color, pos):
         """
         """
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((40, 40))
+        self.image = pygame.Surface((TOKEN_SIZE, TOKEN_SIZE))
         self.screen = screen
         self.pos = pos
         self.color = color
         self.selected = False
         self.rect = pygame.draw.circle(self.screen, self.color, self.pos, self.radius, 0)
-        self.image.fill((255,255,255,255))
+        self.screen.blit(self.image, self.rect)
 
     def _draw(self):
-        self.rect = pygame.draw.circle(self.screen, self.color, self.pos, self.radius, 0)
         if self.selected:
-            self.image.fill((0, 0, 0, 0))
+            self.image.fill(BLACK)
         else:
-            self.image.fill((255, 255, 255, 255))
+            self.image.fill(WHITE)
+        self.rect = pygame.draw.circle(self.screen, self.color, self.pos, self.radius, 0)
 
     def draw(self):
         self._draw()
@@ -150,6 +85,131 @@ class Token(pygame.sprite.Sprite):
         self.rect = to_slot
 
 
+class Game(object):
+    """A game board
+    """
+
+    def __init__(self, mode=(W, H)):
+        """Create and initialize the game board
+        """
+        self.screen = pygame.display.set_mode(mode)
+        self.background = pygame.image.load('images/board.png').convert()
+        self.tokens = {}
+        self.board = Board()
+        self.state = STATE['start']
+        self._setup_messagebar()
+        self.tokens = pygame.sprite.LayeredUpdates()
+        self._setup_tokens()
+        self.active_token = self.tokens.get_sprite(0)
+        self.current_slot = self.board.slots[0]
+        self.current_color = BLUE
+        self.active_token.selected = True
+
+    def _setup_messagebar(self):
+        self.msgsrc = pygame.font.SysFont(None, FONT_SIZE)
+        self.msgdest = pygame.Surface((W, H_MSG))
+        self.msgdest.fill(WHITE)
+        self.surf = self.msgsrc.render("START", True, BLACK)
+        self.msgdest.blit(self.surf, (10, 0))
+        self.screen.blit(self.msgdest, (0, H_BGND))
+
+    def _setup_tokens(self):
+        # set up tokens
+        for t in tokens:
+            token = Token(self.screen, COLOR_VALUE[t[0]], t[1])
+            self.tokens.add(token)
+
+    def msg(self, text):
+        print text
+        self.msgdest.fill(WHITE)
+        s = self.msgsrc.render(text, True, BLACK)
+        self.msgdest.blit(s, (0, 0))
+        self.screen.blit(self.msgdest, (0, H_BGND))
+        pygame.display.flip()
+
+    def token_select(self, token):
+        """Put token in the selected state"""
+        token.selected = True
+
+    def token_move(self, token, to_slot):
+        """Move token to to_slot"""
+        token.move(to_slot)
+
+    def on_key_down(self, event):
+        """Handle keyboard events"""
+        if event.key == K_r:
+            self.msg("Restarting")
+            self.__init__()
+
+        if event.key == K_s:
+            self.msg("Starting game")
+            return
+        if event.key in NUMKEYS:
+            to_slot = self.board.slots[NUMKEYS.index(event.key)]
+            tlist = [i for i in self.tokens.sprites() if i.color == self.current_color]
+            if to_slot.collidelist(tlist) != -1:
+                self.msg("That space is occupied")
+                return
+            self.active_token.move((to_slot.x, to_slot.y))
+            self.active_token.draw()
+            return
+        if event.key == K_n:
+            if self.current_slot.nextslot.token:
+                self.msg("That space is occupied")
+                return
+            self.current_slot = self.current_slot.nextslot
+            self.screen.blit(self.background, (0, 0))
+            self.active_token.move((self.current_slot.x, self.current_slot.y))
+            self.active_token.draw()
+            return
+        if event.key == K_p:
+            if self.current_slot.prevslot.token:
+                self.msg("That space is occupied")
+                return
+            self.current_slot = self.current_slot.prevslot
+            self.screen.blit(self.background, (0, 0))
+            self.active_token.move((self.current_slot.x, self.current_slot.y))
+            self.active_token.draw()
+            return
+        if event.key == K_ESCAPE:
+            self.active_token.selected = False
+        if event.key == K_q:
+            self.on_quit()
+
+    def on_mousebutton_down(self, event):
+        """Handle mouse events"""
+        print "Click: %s" % (event.pos,)
+        # if a token has been selected previously, move it to the destination of the click
+        # as long as the spot is not occupied
+        hits = self.tokens.get_sprites_at(event.pos)
+        if self.active_token.selected:
+            if len(hits) == 1:
+                if hits[0] is self.active_token:
+                    self.msg("To deselect a token, press the ESCAPE key")
+                    return
+                else:
+                    self.msg("That space is occupied")
+                    return
+            elif len(hits) == 0:
+                s = pygame.Rect((event.pos[0], event.pos[1], 20, 20))
+                hit = s.collidelist(self.board.slots)
+                if hit != -1:
+                    self.active_token.selected = False
+                    self.active_token.move((self.board.slots[hit].x, self.board.slots[hit].y))
+                    if self.current_color == BLUE:
+                        self.current_color = GREEN
+                    else:
+                        self.current_color = BLUE
+        else:
+            if len(hits) == 1:
+                self.active_token = hits[0]
+                self.active_token.selected = True
+        return
+
+    def on_quit(self):
+        sys.exit()
+
+
 def main(*args, **kwargs):
     """Start the main loop
 
@@ -157,28 +217,25 @@ def main(*args, **kwargs):
     - `*args`:
     - `**kwargs`:
     """
-    print kwargs
     pygame.init()
-    game = GameBoard()
-    active_token = None
+    game = Game()
     screen = game.screen
-    screen.blit(game.background, (0,0))
-    slot = game.slots[0]
+    screen.blit(game.background, (0, 0))
     while 1:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                game.onKeyDown(event)
+                game.on_key_down(event)
             if event.type == MOUSEBUTTONDOWN:
-                game.onMouseButtonDown(event)
+                game.on_mousebutton_down(event)
             if event.type == QUIT:
-                game.onQuit(event)
-        for color in HOME:
-            game.tokens[color].clear(game.screen, game.background)
-            game.tokens[color].draw(game.screen)
-            game.tokens[color].update()
-        pygame.display.update()
+                game.on_quit()
+        game.tokens.clear(game.screen, game.background)
+        game.tokens.draw(game.screen)
+        game.tokens.update()
+        pygame.display.flip()
         pygame.time.delay(100)
 
+
 if __name__ == '__main__':
-    (opts, args) = parser.parse_args()
-    sys.exit(main(*args, **opts.__dict__))
+    (opts, cli_args) = parser.parse_args()
+    sys.exit(main(*cli_args, **opts.__dict__))
